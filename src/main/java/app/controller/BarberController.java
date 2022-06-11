a3137dc0ebe99aa6b86e481be055008852a9bd79
pjniche <pjniche@gmail.com>
2022-06-11 20:03:48 -0300

    Update BarberController.java

--- src/main/java/app/controller/BarberController.java
+++ src/main/java/app/controller/BarberController.java
@@ -67,8 +67,8 @@ public class BarberController {
   @DeleteMapping(value = "/delete/{barberId}")
   public ResponseEntity deleteBarber(@PathVariable(value = "barberId") long barberId)
       throws JsonProcessingException {
-    BarberService barberSevice = new BarberService(barberRepo);
-    barberSevice.deleteBarber(barberId);
+    BarberService barberService = new BarberService(barberRepo);
+    barberService.deleteBarber(barberId);
     return new ResponseEntity(HttpStatus.OK);
   }
 }
